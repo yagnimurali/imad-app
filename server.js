effec6787f2c085a5cc626359f12e8ad1c414c4d
yagnimurali <yagni99@gmail.com>
2017-08-03 23:41:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*var express = require('express');
+var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
@@ -25,7 +25,7 @@ var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-*/
+
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
